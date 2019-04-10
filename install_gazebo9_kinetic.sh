7d156f9a70661fea768607f0d2826888675febd6
Adam Conkey <adam.conkey@utah.edu>
2019-04-09 19:41:41 -0600

    Fix typos

--- install_gazebo9_kinetic.sh
+++ install_gazebo9_kinetic.sh
@@ -18,7 +18,7 @@ hg clone https://bitbucket.org/osrf/gazebo
 
 hg clone https://bitbucket.org/osrf/sdformat -r sdf6
 
-hg clone https://bitbucket.org/ignitionrobotics/ign-math -r ign-math4
+hg clone https://bitbucket.org/ignitionrobotics/ign-tools
 
 
 
@@ -28,7 +28,7 @@ sudo apt install libignition-transport4-dev
 
 URL_PREFIX="https://raw.githubusercontent.com/adamconkey/setup_scripts/master/package_xml/"
 curl ${URL_PREFIX}gazebo9_package.xml > ${WORKSPACE_PATH}/src/gazebo/package.xml
-curl ${URL_PREFIX}ign_tools_package.xml > ${WORKSPACE_PATH}/src/ign_tools/package.xml
+curl ${URL_PREFIX}ign_tools_package.xml > ${WORKSPACE_PATH}/src/ign-tools/package.xml
 curl ${URL_PREFIX}sdformat_package.xml > ${WORKSPACE_PATH}/src/sdformat/package.xml
 
 
