eb21b79b22fe9a133b3a05411650efe505694003
Adam Conkey <adam.conkey@utah.edu>
2019-04-13 17:56:37 -0600

    Add documentation for gazebo9 script

--- install_gazebo9_kinetic.sh
+++ install_gazebo9_kinetic.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
+#===================================================================#
+# This script installs Gazebo9 from source into a catkin workspace.
+# By default it will also install DART from source in the same
+# catkin workspace to enable Gazebo+DART physics engine. You can
+# configure the installation by setting the boolean options below,
+# as well as defining the where the workspace will be created.
+#
+# It is recommended that you keep this workspace isolated, and
+# extend it in your other workspaces. For example, you can config
+# your other workspaces with the following command (assuming you
+# adopted the default workspace name provided by this script):
+#
+#     catkin config --extend $HOME/gazebo9_ws
+#
+# Once you explicitly extend in a workspace you should do a clean
+# build of it.
+#===================================================================#
 
-INSTALL_ROS=false
-INSTALL_GAZEBO=true        # Install Gazebo9 from source
-INSTALL_GAZEBO_ROS=true    # Install gazebo_ros_pkgs from source
-INSTALL_DART=true          # Install Dart6.7 from source
-INSTALL_DART_OPTIONAL=true # Install optional Dart dependencies (apt)
-INSTALL_DEPENDENCIES=true  # Install dependencies with apt (recommended)
+
+INSTALL_DEPENDENCIES=true  # Install apt dependencies (recommended)
+INSTALL_DART=true          # Install DART from source
+INSTALL_DART_OPTIONAL=true # Install optional DART dependencies
 
 # Set the path location where you want the catkin workspace created:
 WORKSPACE_PATH="$HOME/gazebo9_ws"
@@ -28,11 +43,10 @@ mkdir -p ${WORKSPACE_PATH}/src
 cd ${WORKSPACE_PATH}/src
 
 
-#------------------------------------------------------------------#
-# Install dependencies for the packages specified to be installed  #
-#------------------------------------------------------------------#
+#-------------------------------------------------------------------#
+# Install dependencies for the packages specified to be installed   #
+#-------------------------------------------------------------------#
 if [ "$INSTALL_DEPENDENCIES" = true ] ; then
-    
     # Setup the necessary apt keys
     sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
     wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
@@ -40,12 +54,9 @@ if [ "$INSTALL_DEPENDENCIES" = true ] ; then
     wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
     
     # Populate packages needed by each component
-    PACKAGES="mercurial python-catkin-tools curl"
-
-    if [ "$INSTALL_ROS" = true ] ; then
-	PACKAGES+=" ros-kinetic-desktop"
-    fi
+    PACKAGES="mercurial python-catkin-tools curl ros-kinetic-desktop"
     
+    # DART dependencies
     if [ "$INSTALL_DART" = true ] ; then
 	PACKAGES+=" build-essential cmake pkg-config git"
 	PACKAGES+=" libeigen3-dev libassimp-dev libccd-dev"
@@ -57,51 +68,37 @@ if [ "$INSTALL_DEPENDENCIES" = true ] ; then
 	    PACKAGES+=" libflann-dev libtinyxml2-dev liburdfdom-dev"
 	    PACKAGES+=" libxi-dev libxmu-dev freeglut3-dev"
 	    PACKAGES+=" libopenscenegraph-dev"
-     	fi
+	fi
     fi
-    
-    if [ "$INSTALL_GAZEBO" = true ] ; then
-	# Run Gazebo's provided dependency install script
-	sudo apt update
-	wget https://bitbucket.org/osrf/release-tools/raw/default/jenkins-scripts/lib/dependencies_archive.sh -O /tmp/dependencies.sh
-	GAZEBO_MAJOR_VERSION=9 ROS_DISTRO=kinetic . /tmp/dependencies.sh
-	echo $BASE_DEPENDENCIES $GAZEBO_BASE_DEPENDENCIES | tr -d '\\' | xargs sudo apt install -y
-    fi
-    
-    if [ "$INSTALL_GAZEBO_ROS" = true ] ; then
-    	PACKAGES+=" ros-kinetic-perception ros-kinetic-ros-control ros-kinetic-ros-controllers"
-    fi
+
+    # Gazebo dependencies. Run Gazebo's installation script.
+    sudo apt update
+    wget https://bitbucket.org/osrf/release-tools/raw/default/jenkins-scripts/lib/dependencies_archive.sh -O /tmp/dependencies.sh
+    GAZEBO_MAJOR_VERSION=9 ROS_DISTRO=kinetic . /tmp/dependencies.sh
+    echo $BASE_DEPENDENCIES $GAZEBO_BASE_DEPENDENCIES | tr -d '\\' | xargs sudo apt install -y
+    # Gazebo ROS dependencies
+    PACKAGES+=" ros-kinetic-perception ros-kinetic-ros-control ros-kinetic-ros-controllers"
     
     sudo apt update
     sudo apt install -y $PACKAGES
 fi
 
 
-#-----------------------------------------------------------------#
-# Clone the repositories at the necessary branches                #
-#-----------------------------------------------------------------#
+#-------------------------------------------------------------------#
+# Clone the repositories at the necessary branches                  #
+#-------------------------------------------------------------------#
 if [ "$INSTALL_DART" = true ] ; then
     git clone https://github.com/dartsim/dart.git -b release-6.7
 fi
-
-if [ "$INSTALL_GAZEBO" = true ] ; then
-    hg clone https://bitbucket.org/osrf/gazebo
-    hg clone https://bitbucket.org/osrf/sdformat -r sdf6
-    hg clone https://bitbucket.org/ignitionrobotics/ign-tools
-    URL_PREFIX="https://raw.githubusercontent.com/adamconkey/setup_scripts/master/package_xml/"
-    curl ${URL_PREFIX}gazebo9_package.xml > ${WORKSPACE_PATH}/src/gazebo/package.xml
-    curl ${URL_PREFIX}ign_tools_package.xml > ${WORKSPACE_PATH}/src/ign-tools/package.xml
-    curl ${URL_PREFIX}sdformat_package.xml > ${WORKSPACE_PATH}/src/sdformat/package.xml
-fi
-
-if [ "$INSTALL_GAZEBO_ROS" = true ] ; then
-    git clone https://github.com/ros-simulation/gazebo_ros_pkgs.git -b kinetic-devel
-fi
+git clone https://github.com/ros-simulation/gazebo_ros_pkgs.git -b kinetic-devel
+hg clone https://bitbucket.org/osrf/gazebo
+URL_PREFIX="https://raw.githubusercontent.com/adamconkey/setup_scripts/master/package_xml/"
+curl ${URL_PREFIX}gazebo9_package.xml > ${WORKSPACE_PATH}/src/gazebo/package.xml
 
 
-#-----------------------------------------------------------------#
-# Initialize the catkin workspace and run the build               #
-#-----------------------------------------------------------------#
+#-------------------------------------------------------------------#
+# Initialize the catkin workspace and run the build                 #
+#-------------------------------------------------------------------#
 source /opt/ros/kinetic/setup.bash
 
 cd ${WORKSPACE_PATH}
